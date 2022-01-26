c1fa81dc1cf1b415671a1aad58f1fcfcedc94d4d
jaewoohwang <79906997+jaewoohwang@users.noreply.github.com>
2022-01-26 19:22:45 +0900

    added cech complex

--- src/PersistenceDiagram.jl
+++ src/PersistenceDiagram.jl
@@ -1,8 +1,18 @@
-using Ripserer, Plots, DataFrames
+using Ripserer, ComputationalHomology, Plots
 
 function PD(data, Backend)
+
     if Backend=="Vietoris-Rips"
         data = Tuple.(eachrow(data));
         plot(ripserer(data))
     end
+
+    if Backend=="Cech"
+        data = Matrix(data);
+        ɛ = 0.01;
+        cplx, w = čech(data, ɛ);
+        pd = cplx |> filtration |> diagram;
+        plot(pd, ms=4)
+    end
+    
 end
