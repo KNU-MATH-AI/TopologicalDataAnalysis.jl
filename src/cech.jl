889112b7f90aa2ff5aedcb47dca31b1363bf2fae
DaeSick <rmsmsgood@naver.com>
2022-03-07 01:14:26 +0900

    backup

--- src/cech.jl
+++ src/cech.jl
@@ -1,6 +1,8 @@
 using BoundingSphere
 using NearestNeighbors
 using Plots
+using DynamicsPlots
+
 
 struct AbstractSimplicialComplex
     Simplex::Set{Int64}
@@ -8,49 +10,70 @@ end
 const ASC = AbstractSimplicialComplex
 
 include("tDATAsets.jl")
-M2n = rand(Sphere(2), 10)
+M2n = rand(Sphere(2), 20)
 d, n = size(M2n)
-x,y = eachrow(M2n); scatter(x,y, text = 1:n)
 ε = 0.3
+maxdim = 4
 
-
-
+# Initialization
 kdtree = KDTree(M2n)
 knn0 = knn(kdtree, M2n, 2)
 bit_Δ0 = first.(knn0[2]) .≥ ε
-Δ0 = [[k] for k in 1:n][bit_Δ0]
-preΔ1 = knn0[1][.!bit_Δ0] .|> sort |> unique
-v2 = reduce(union, preΔ1)
+Δ0 = [[j] for j in 1:n][bit_Δ0]
 
-Δ1 = []
-preΔ2 = []
-for (idx_taboo, v0v1) in enumerate(preΔ1)
-    flag_Δ1 = false
-    for Δ2 in preΔ2
-        if v0v1 ⊆ Δ2
-            flag_Δ1 = true
-            continue
+
+begin
+Δ = []
+preΔ = []
+push!(preΔ, knn0[1][.!bit_Δ0] |> sort .|> sort |> unique)
+for k in 1:maxdim
+    if preΔ[k] |> isempty break end
+    push!(Δ, [])
+    push!(preΔ, [])
+    flatΔ = reduce(∪, preΔ[k]) |> sort
+    for (idx_taboo, β) in enumerate(preΔ[k])
+        flag_Δ = false
+        for α in preΔ[k+1]
+            if β ⊆ α
+                flag_Δ = true
+                break
+            end
         end
+        if flag_Δ continue end
+        for v in flatΔ[idx_taboo:end]
+            if v in β
+                continue
+            end
+            println("candy - β: ", β, " ∪  {", v, "}")
+            candy = [β; v] |> sort # `candy` is a alias of 'candidate'.
+            c, r = boundingsphere([M2n[:,idx] for idx in candy])
+            if r ≤ ε && candy == inrange(kdtree, c, r + eps(), true)
+                flag_Δ = true
+                push!(preΔ[k+1], candy)
+                continue
+            end
+        end
+        if flag_Δ continue end
+        push!(Δ[k], β)
+        println(Δ[k])
     end
-    if flag_Δ1 continue end
-    for v in v2[idx_taboo:end]
-        if v in v0v1
-            continue
-        end
-        v0v1v2 = [v0v1; v]
-        println("candidate: ", v0v1v2)
-        c, r = boundingsphere([M2n[:,idx] for idx in v0v1v2])
-        if r ≤ ε && v0v1v2 == inrange(kdtree, c, r + eps(), true)
-            flag_Δ1 = true
-            println(inrange(kdtree, c, r + eps(), true))
-            push!(preΔ2, v0v1v2)
-            break
-        end
-    end
-    push!(Δ1, v0v1)
 end
+end
+Δ0
+Δ
+preΔ[end]
+union(reduce(∪, Δ0), reduce(∪, reduce(∪, Δ))) |> sort
 
-Δk = copy(Δ1)
-preΔkpp = []
+candy = [5,8,11,12]
+c, r = boundingsphere([M2n[:,idx] for idx in candy])
+inrange(kdtree, c, r + eps(), true)
 
-cech(x) = x .^ 2
+color = [:yellow, :gray, :orange, :red, :black, :purple]
+x,y = eachrow(M2n); p1 = scatter(x,y, text = 1:n, size = (600,600), color = :white)
+for i in 1:length(Δ)
+    for j in Δ[i]
+        c, r = boundingsphere([M2n[:,k] for k in j])
+        circle!(c,r, color = color[i], label = :none)
+    end
+end
+circle!((0,0), ε, color = :blue, legend= :none)
