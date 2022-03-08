34ad4026c52095a34529d2ac3a56405823f3efea
jaewoohwang <jaewoo7932@knu.ac.kr>
2022-03-08 15:14:56 +0900

    Remove contents of ripserer.jl and ComputationalHomology.jl

--- src/PersistenceDiagram.jl
+++ src/PersistenceDiagram.jl
@@ -1,46 +1,14 @@
-using Ripserer, PersistenceDiagrams, Plots
-include("ComputationalHomology.jl")
+using Plots
 
 function PD(; 
     data, 
-    complex, 
-    dim_max=1,
-    cutoff=0,
-    verbose=false,
-    alg=:cohomology,
-    reps=alg == :cohomology ? false : 1:dim_max,
-    implicit=alg != :homology,
-    weights = true,
-    ɛ = 1,
-    filtration_weights = true
+    complex
     )
 
     if complex=="Vietoris-Rips"
-        pd = ripserer(Tuple.(eachrow(data)), 
-        dim_max=dim_max, 
-        cutoff=cutoff, 
-        verbose=verbose,
-        alg=alg, 
-        reps=reps, 
-        implicit=implicit)
-        return plot(pd)
     end       
 
     if complex=="Cech"
-        cplx, w = ComputationalHomology.čech(Matrix(data), ɛ, filtration_weights);
-        pd = ComputationalHomology.diagram(ComputationalHomology.filtration(cplx));
-
-        temp = NTuple{2}[];
-        pd_converted = Array{PersistenceDiagram, 1}();
-
-        for dim = 0:length(pd)-1
-            for int_num = 1:length(pd[dim])
-                push!(temp, (pd[dim][int_num].b, pd[dim][int_num].d))
-            end
-            push!(pd_converted, PersistenceDiagram(PersistenceInterval.(temp)));
-        end
-
-        return plot(pd_converted)
     end
 end
 
