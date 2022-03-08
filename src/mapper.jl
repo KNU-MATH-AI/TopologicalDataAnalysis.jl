2989aa80041d06274881e20b91eca6844df8aca5
GH J <bbbcaobb@gmail.com>
2022-03-08 20:04:26 +0900

    develop mapper

--- src/mapper.jl
+++ src/mapper.jl
@@ -1,123 +1,7 @@
-using StatsPlots 
-using Clustering 
-using Distances 
-using Distributions
-using DataFrames
-
-import Base: ∈
-import Base: ∩
-
-include("../src/utilities/utils.jl")
-
-# a = rand(Uniform(0,.8), 2, 20)
-# b = rand(Uniform(1.2,2), 2, 20)
-# c = a + cat(1.2ones(1,20),zeros(1,20), dims=1)
-# X = cat(a,b,c, dims=2)
-
-# scatt = scatter(X[1,:], X[2,:], label=false)
-
-# D_a = pairwise(Euclidean(), X, X)
-# SL = hclust(D_a, linkage=:single)
-# CL = hclust(D_a, linkage=:complete)
-# AV = hclust(D_a, linkage=:average)
-# plot(SL)
-# plot(CL)
-# plot(AV)
-
-# propertynames(Hclust)
-
-# propertynames(SL)
-# SL.merges
-# SL.heights
-# SL.order
-# SL.linkage
-# SL.height
-# SL.labels
-# SL.merge
-# SL.method
-
-# plot(SL.heights)
-# SL.heights[56]
-# SL.heights[57]
-# SL.heights[58]
-# SL.heights[59]
-
-# kmeans_ = kmeans(reshape(SL.heights, (1,59)), 2)
-# kmeans_ = kmeans(reshape(CL.heights, (1,59)), 2)
-# propertynames(kmeans_)
-# @show kmeans_
-
-# cutree(SL, k=2)
-
-# D_SL = pairwise(Euclidean(), SL.heights)
-# dbscan(reshape(SL.heights, (1,59)), 0.8maximum(SL.heights))
-# dbscan(reshape(SL.heights, (1,59)), 0.05maximum(SL.heights))
-# dbscan(reshape(SL.heights, (1,59)), 0.2maximum(SL.heights))
-
-# dbscan_ = dbscan(reshape(CL.heights, (1,59)), 0.1maximum(CL.heights))
-# dbscan_[1]
-# dbscan_[2]
-# dbscan(reshape(CL.heights, (1,59)), 0.08maximum(CL.heights))
-# dbscan(reshape(CL.heights, (1,59)), 0.2maximum(CL.heights))
-
-# cutree(CL, h=CL.heights[58])
-# x = Interval
-# x = Interval(1,2)
-
-# struct OrderedPair
-#     x::Real
-#     y::Real
-#     OrderedPair(x,y) = x > y ? error("out of order") : new(x,y)
-# end
-# OrderedPair(3,1)
-
-# I = Interval(0.2, 4.0)
-# -1 ∈ I
-# 1 ∈ I
-# J = Interval(-1., 3.6)
-
-# # function intersection(I::Interval, J::Interval)
-# #     min = maximum([I.a, J.a])
-# #     max = minimum([I.b, J.b])
-
-# #     return Interval(min, max)
-# # end
-
-# ∩(I,J)
-
-begin
-using StatsPlots 
-using Clustering 
-using Distances 
-using Distributions
-using DataFrames
-
-import Base: ∈
-import Base: ∩
-
-include("../src/utilities/utils.jl")
-Data = noisy_circle_data(500, 0.03)
-
-function proj_1d(X)
-    min = minimum(X[1, :])
-    output = X[1, :] .- min
-    
-    return output
-end
-
 struct Interval{T<:Real}
     a::T
     b::T
     # Interval(a,b) = a > b ? error("out of order") : new(a,b)
-end
-
-mutable struct MapperInformations
-    data_number
-    original_coordinate
-    original_subset
-    clustered_subset
-    filtered_coordinate
-    interval_subset
 end
 
 function ∈(x::T, interval::Interval) where T<:Real
@@ -135,11 +19,79 @@ function ∩(I::Interval, J::Interval)
     return Interval(min, max)
 end
 
-function filtered_coordinate(x, p)
-    x.filtered_coordinate = p
+mutable struct MapperInformations
+    data_number
+    original_coordinate
+    original_subset
+    clustered_subset
+    filtered_coordinate
+    interval_subset
 end
 
-#Y_ = MapperInformations(:None, :None, :None, :None, :None)
+function interval_test(Info, Intervals)
+    M = length(Info)
+    N = length(Intervals)
+    
+    X_subset = [[] for i ∈ 1:N]
+    for i ∈ 1:M
+        for j ∈ 1:N
+            if Info[i].filtered_coordinate ∈ Intervals[j]
+                Info[i].interval_subset = Intervals[j]
+                Info[i].original_subset = push!(Info[i].original_subset, j)
+                push!(X_subset[j], Info[i].original_coordinate)
+            end
+        end
+    end
+
+    return X_subset
+end
+
+function domain_clustering(Info, X_subset)
+        
+    X_subset_clustering = []
+
+    for i ∈ 1:length(X_subset)
+        # i=3
+        temp = zeros(length(X_subset[i][1]), length(X_subset[i]))
+        for j ∈ 1:length(X_subset[i])
+            temp[:, j] = X_subset[i][j]
+        end
+        scatter(temp[1,:], temp[2,:])               
+
+        D = pairwise(Euclidean(), temp, temp)
+        
+        SL = hclust(D, linkage=:single)
+        plot(SL)
+        
+        dbsacn_temp = dbscan(reshape(SL.heights, (1,length(SL.heights))), 0.2maximum(SL.heights))
+        cutree_temp = cutree(SL, h=SL.heights[dbsacn_temp[1].core_indices[end]])
+        
+        X_subset_clustering_temp = [[] for j ∈ 1:maximum(cutree_temp)]
+        
+        for n ∈ 1:length(X_subset[i])
+            clustering_index = cutree_temp[n]
+            data_index = findfirst(==(X_subset[i][n]), original_X)
+            push!(X_subset_clustering_temp[clustering_index], data_index)                
+            Info[data_index].clustered_subset = push!(Info[data_index].clustered_subset, (i, clustering_index))
+        end
+
+        push!(X_subset_clustering, X_subset_clustering_temp)
+        
+    end
+    return X_subset_clustering
+end
+
+function coor_mean(X_subset_clustering, Info)
+    center_ = [ [] for i ∈ 1:length(X_subset_clustering)]
+    for i ∈ 1:length(X_subset_clustering)
+        for j ∈ 1:length(X_subset_clustering[i])
+            temp = [Info[v].original_coordinate for v ∈ X_subset_clustering[i][j]]
+            center_vector = sum(temp)./length(temp)
+            push!(center_[i], center_vector)
+        end
+    end
+
+    return center_
 end
 
 function mapper(X, filter, interval, overlab)
@@ -251,31 +203,3 @@ function mapper_plot(link, X_subset_clustering, center_, zcolor, xlim, ylim)
 
     return p
 end
-
-
-d1 = scatter(Data[1,:], Data[2,:], label="")
-d2 = scatter(Data[1,:], Data[2,:], zcolor=proj_1d(Data), color=:jet, label="")
-
-link, X_subset_clustering, Info, center_, zcolor = mapper(Data, proj_1d, 5, 0.2)
-p = mapper_plot(link, X_subset_clustering, center_, zcolor, (-1.5, 1.5), (-1.5,1.5))
-
-plot(d1, d2, p, size=(1400,800))
-
-# plot(d, p, size=(1000,400))
-# include("../src/utilities/tDATAsets.jl")
-
-# Data = rand(Sphere(2), 500)
-# Data = cat(rand(Infty(), 500), rand(Sphere(2), 500).+1, dims=2)
-
-
-# link, X_subset_clustering, Info = mapper(Data, proj_1d, 5, 0.3)
-# p = mapper_plot(link, X_subset_clustering, Info, (-1.5, 1.5), (-1.5,1.5))
-
-# plot(d, p, size=(1000,400))
-
-
-# d = scatter(Data[1,:], Data[2,:])
-# link, X_subset_clustering, Info = mapper(Data, proj_1d, 10, 0.2)
-# p = mapper_plot(link, X_subset_clustering, Info)
-
-# plot(d, p, size=(1000,400))
