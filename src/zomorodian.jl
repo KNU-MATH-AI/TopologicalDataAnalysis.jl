177c51e2a76c02002d129110edd62b5cff6c42ae
DaeSick <rmsmsgood@naver.com>
2022-06-26 01:10:22 +0900

    Backup for L_ before using dictionary

---
+++ src/zomorodian.jl
@@ -0,0 +1,87 @@
+using DataFrames
+
+data = DataFrame([
+0 "a"
+0 "b"
+1 "c"
+1 "d"
+1 "ab"
+1 "bc"
+2 "cd"
+2 "ad"
+3 "ac"
+4 "abc"
+5 "acd"
+], ["epsilon", "sigma"])
+T = copy(data)
+T[!, :"marked"] .= false
+T[!, :"d"] .= [[]]
+T[!, :"idx2"] .= 0
+
+dimK = 2
+m = nrow(T)
+
+test_σ = "abc"
+dim(σ) = length(σ)
+
+function deg(σ)
+    return T.epsilon[findfirst(T.sigma .== σ)]
+end
+deg(test_σ)
+
+function ∂(σ)
+    k = dim(σ)
+    return [σ[(1:k)[Not(t)]] for t = 1:k]
+end
+∂(test_σ)
+
+function maxindex(chain)
+    return (T.sigma .∈ Ref(chain)) |> findall |> maximum
+end
+maxindex(∂(test_σ))
+
+
+function REMOVEPIVOTROWS(σ)
+    k = dim(σ); d = ∂(σ)
+    d = d[d .∈ Ref(T[T.marked,:sigma])] # Remove unmarked terms in $d$
+    while !(d |> isempty)
+        i = maxindex(d)
+        if T[i,:d] |> isempty break end
+        d = symdiff(d, T[i,:d])
+        # print("d in empty")
+    end
+    return d
+end
+REMOVEPIVOTROWS(test_σ)
+
+# coumpute_intervals
+# L_ = Dict(); for k = 0:dimK L_[k] = [] end
+
+T = copy(data)
+T[!, :"marked"] .= false
+T[!, :"d"] .= [[]]
+T[!, :"idx2"] .= 0
+
+L_ = [[] for k = 0:dimK]
+for j0 = 0:(m-1)
+    j = j0+1
+    σʲ = T[j,:sigma]
+    d = REMOVEPIVOTROWS(σʲ)
+    if d |> isempty
+        T[j,:marked] = true
+    else
+        i = maxindex(d); k = dim(σʲ)
+        σⁱ = T[i,:sigma]
+        T[i,[:idx2,:d]] = j,d
+        L_[k] = L_[k] ∪ [(deg(σⁱ), deg(σʲ))]
+    end
+end
+for j0 = 0:(m-1)
+    j = j0+1
+    σʲ = T[j,:sigma]
+    if (T[j,:marked]) && (T[j,:d] |> isempty)
+        k = dim(σʲ); L_[k] = L_[k] ∪ [(deg(σʲ), Inf)]
+        print("j: $j")
+    end
+end
+L_
