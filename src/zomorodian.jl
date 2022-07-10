f522a1eebd0fa2cc063ffb29c813c03067d66102
DaeSick <rmsmsgood@naver.com>
2022-07-10 22:12:44 +0900

    variable names changed

--- src/zomorodian.jl
+++ src/zomorodian.jl
@@ -12,11 +12,11 @@ data = DataFrame([
 3 "ac"
 4 "abc"
 5 "acd"
-], ["epsilon", "sigma"])
+], ["epsilon", "simplex"])
 T = copy(data)
 T[!, :"marked"] .= false
-T[!, :"d"] .= [[]]
-T[!, :"idx2"] .= 0
+T[!, :"slot"] .= [[]]
+T[!, :"J"] .= 0
 
 dimK = 2
 m = nrow(T)
@@ -25,7 +25,7 @@ test_σ = "abc"
 dim(σ) = length(σ)
 
 function deg(σ)
-    return T.epsilon[findfirst(T.sigma .== σ)]
+    return T.epsilon[findfirst(T.simplex .== σ)]
 end
 deg(test_σ)
 
@@ -36,18 +36,18 @@ end
 ∂(test_σ)
 
 function maxindex(chain)
-    return (T.sigma .∈ Ref(chain)) |> findall |> maximum
+    return (T.simplex .∈ Ref(chain)) |> findall |> maximum
 end
 maxindex(∂(test_σ))
 
 
 function REMOVEPIVOTROWS(σ)
     k = dim(σ); d = ∂(σ)
-    d = d[d .∈ Ref(T[T.marked,:sigma])] # Remove unmarked terms in $d$
+    d = d[d .∈ Ref(T[T.marked,:simplex])] # Remove unmarked terms in $d$
     while !(d |> isempty)
         i = maxindex(d)
-        if T[i,:d] |> isempty break end
-        d = symdiff(d, T[i,:d])
+        if T[i,:slot] |> isempty break end
+        d = symdiff(d, T[i,:slot])
         # print("d in empty")
     end
     return d
@@ -59,27 +59,27 @@ REMOVEPIVOTROWS(test_σ)
 
 T = copy(data)
 T[!, :"marked"] .= false
-T[!, :"d"] .= [[]]
-T[!, :"idx2"] .= 0
+T[!, :"slot"] .= [[]]
+T[!, :"J"] .= 0
 
 L_ = [[] for k = 0:dimK]
 for j0 = 0:(m-1)
     j = j0+1
-    σʲ = T[j,:sigma]
+    σʲ = T[j,:simplex]
     d = REMOVEPIVOTROWS(σʲ)
     if d |> isempty
         T[j,:marked] = true
     else
         i = maxindex(d); k = dim(σʲ)
-        σⁱ = T[i,:sigma]
-        T[i,[:idx2,:d]] = j,d
+        σⁱ = T[i,:simplex]
+        T[i,[:J,:slot]] = j0,d
         L_[k] = L_[k] ∪ [(deg(σⁱ), deg(σʲ))]
     end
 end
 for j0 = 0:(m-1)
     j = j0+1
-    σʲ = T[j,:sigma]
-    if (T[j,:marked]) && (T[j,:d] |> isempty)
+    σʲ = T[j,:simplex]
+    if (T[j,:marked]) && (T[j,:slot] |> isempty) && (T[j,:J] |> iszero)
         k = dim(σʲ); L_[k] = L_[k] ∪ [(deg(σʲ), Inf)]
         print("j: $j")
     end
