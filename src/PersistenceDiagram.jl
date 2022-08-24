ca9b3c14b3a796ed2a496dedee218c18ed4d4880
jaewoohwang <jaewoo7932@knu.ac.kr>
2022-08-24 14:53:19 +0900

    Implementation of persistence diagram (vietoris-rips)

--- src/PersistenceDiagram.jl
+++ src/PersistenceDiagram.jl
@@ -1,15 +1,32 @@
 using Plots
 
-function PD(; 
-    data, 
-    complex
-    )
+function persistencediagram(p_intervals, filtered_complex, Infval)
 
-    if complex=="Vietoris-Rips"
-    end       
+    δ_table = unique(filtered_complex[:,[1,2]], :degree)
+    δ_dict = Dict(δ_table.degree .=> δ_table.appearance)
+    
+    lim = maximum(δ_table.appearance)
 
-    if complex=="Cech"
+    fig = plot([0,lim], [0, lim],
+        xlabel = "birth",
+        ylabel = "death",
+        label =:none,
+        color = "black"
+        )
+    
+    for k in 0:Infval
+        if !isempty(get.(Ref(δ_dict), first.(p_intervals[k]), Infval))
+            fig = scatter!(fig,
+            get.(Ref(δ_dict), first.(p_intervals[k]), Infval),
+            get.(Ref(δ_dict), last.(p_intervals[k]), Infval),
+            markeralpha = 1,
+            markershape = :x,
+            label = "H$k",
+            legend=:outertopright
+            )
+        end
     end
+    return fig
 end
 
 
--- test/test_PersistenceDiagram.jl
+++ test/test_PersistenceDiagram.jl
@@ -1,1 +1,20 @@
 include("../src/PersistenceDiagram.jl")
+include("../src/utilities/tDATAsets.jl")
+include("../src/complices/VR.jl")
+include("../src/homology/zomorodian.jl")
+
+using Random
+using Plots
+
+Random.seed!(0)
+point_colud = rand(Sphere(2), 20)
+
+@time fc = VR(point_colud)
+p_intervals = zomorodian(fc)
+
+δ_table = unique(fc[:,[1,2]], :degree)
+δ_dict = Dict(δ_table.degree .=> δ_table.appearance)
+
+Infval = 2
+
+p = persistencediagram(p_intervals, fc, Infval)
